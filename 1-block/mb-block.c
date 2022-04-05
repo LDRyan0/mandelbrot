f05e329b4472deccdfa1e957b11455bf842aadda
Liam Ryan <liamdryan01@gmail.com>
2022-04-06 03:55:30 +0800

    csv output block

--- 1-block/mb-block.c
+++ 1-block/mb-block.c
@@ -9,34 +9,41 @@
 
 /* ----------------------------------------------------------------*/
 
-int main(int argc, char*argv[]) {
-    int	   i, j, k, loop, groupSize, rank, ncpu, pos;
-    float  *x, *y;
-    float complex   z, kappa;
+int main(int argc, char*argv[]){
+	int i, j, k, loop, groupSize, rank, ncpu, pos;
+    float *x, *y;
+    float complex z, kappa;
+	FILE *fp;
 
+	/* Unused variables cause errors in compilation due to -Werror standard*/
 	#ifndef TIME
-    FILE   *fp;
 	int green, blue;
 	#endif
 
 	#ifdef TIME
-	double time1, time2, calcTime, waitTime, commTime;
+	double times[4]; /* [rank, calcTime, waitTime, commTime] */
+	double* allTimes;
+	double time1, time2;
 	#endif
 
 	/* Set up MPI */
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &ncpu);
-	
-	groupSize = N * N / ncpu;
+
+	groupSize = N * N / ncpu; /* Assumes perfect multiple */
 
 	/* Allocate arrays */ 
     x=(float *)malloc(groupSize*sizeof(float));	
 	if (rank == 0) {
 		y = (float *)malloc(N*N*sizeof(float));	
+		#ifdef TIME
+		allTimes = (double *)malloc(4*ncpu*sizeof(double));
+		#endif
 	}
 	
 	#ifdef TIME
+	times[0] = (double)rank;
 	time1 = MPI_Wtime();
 	#endif
     for (loop=0; loop<groupSize; loop++) {
@@ -54,29 +61,32 @@ int main(int argc, char*argv[]) {
 
 	#ifdef TIME
 	time2 = MPI_Wtime();
-	calcTime = time2 - time1;
+	times[1] = time2 - time1; /* calcTime */
 	MPI_Barrier(MPI_COMM_WORLD);
 	time1 = MPI_Wtime();
-	waitTime = time1 - time2;
+	times[2] = time1 - time2; /* waitTime */
 	#endif
 
 	MPI_Gather(x, groupSize, MPI_FLOAT, y, groupSize, MPI_FLOAT, 0, MPI_COMM_WORLD);
 	
 	#ifdef TIME
 	time2 = MPI_Wtime();
-	commTime = time2 - time1;
-	printf("Process %d\n", rank);
-	printf("\tCalc: %lf s\n", calcTime);
-	printf("\tWait: %lf s\n", waitTime);
-	printf("\tComm: %lf s\n", commTime);
+	times[3] = time2 - time1; /* commTime */
+	MPI_Gather(&times, 4, MPI_DOUBLE, allTimes, 4, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 	#endif
-
-
-
-/* ----------------------------------------------------------------*/
 	
-	#ifndef TIME
-	if(rank == 0) { 
+	if (rank == 0) {
+		#ifdef TIME /* Write timing results to .csv file */
+		fp = fopen("block-results.csv","w");
+		fprintf(fp, "rank,calcTime,waitTime,commTime\n"); /* .csv header */
+		for (i = 0; i < 4*ncpu; i+=4) {
+			fprintf(fp, "%d,%lf,%lf,%lf\n", (int)allTimes[i], allTimes[i+1], allTimes[i+2], allTimes[i+3]);
+		}
+		fclose(fp);
+		free(allTimes);
+		#endif
+
+		#ifndef TIME /* Write mandelbrot image to .ppm file */
 	    printf("Writing mandelbrot1.ppm\n");
 	    fp = fopen ("mandelbrot1.ppm", "w");
 	    fprintf (fp, "P3\n%4d %4d\n255\n", N, N);
@@ -89,16 +99,16 @@ int main(int argc, char*argv[]) {
 		        blue= (int) (2*y[loop]*255-255);
 	            fprintf (fp, "%3d\n%3d\n%3d\n", 0,255-blue,blue);
 		    }
-	    
 	    fclose(fp);
+	    #endif
+
 		free(y);
 	}
-	#endif
-
-/* ----------------------------------------------------------------*/
-
+	
     free(x);
 
 	MPI_Finalize();
 	return 0;
 }
+
+
--- 3-cyclic/mb-cyclic.c
+++ 3-cyclic/mb-cyclic.c
@@ -1,5 +1,5 @@
 #define MAXITER 1000
-#define N 8000
+#define N 800
 
 #include <mpi.h>
 #include <stdio.h>
@@ -13,15 +13,17 @@ int main(int argc, char*argv[]) {
     int	   i, j, k, loop, procSize, rank, ncpu, pos;
     float  *x, *y, *out;
     float complex   z, kappa;
+	FILE   *fp; /* for image .ppm or timing .csv file */
 
 	/* Unused variables cause errors in compilation due to -Werror standard*/
 	#ifndef TIME
-	FILE   *fp;
 	int green, blue;
 	#endif
 
 	#ifdef TIME
-	double time1, time2, calcTime, waitTime, commTime;
+	double times[4];
+	double *allTimes;
+	double time1, time2;
 	#endif
 
 	/* Set up MPI */
@@ -35,9 +37,13 @@ int main(int argc, char*argv[]) {
 	if (rank == 0) {
 		y = (float *)malloc(N*N*sizeof(float));
 		out = (float *)malloc(N*N*sizeof(float));
+		#ifdef TIME
+		allTimes = (double *)malloc(4*ncpu*sizeof(double));
+		#endif
 	}
 
 	#ifdef TIME
+	times[0] = (double)rank;
 	time1 = MPI_Wtime();
 	#endif
     for (loop=0; loop < procSize; loop++) {
@@ -53,11 +59,11 @@ int main(int argc, char*argv[]) {
 	    x[loop]= log((float)k) / log((float)MAXITER);
 	}
 	#ifdef TIME
-	time2 = MPI_Wtime();
-	calcTime = time2 - time1;
+	time2 = MPI_Wtime(); /* calcTime */
+	times[1] = time2 - time1;
 	MPI_Barrier(MPI_COMM_WORLD);
 	time1 = MPI_Wtime();
-	waitTime = time1 - time2;
+	times[2] = time1 - time2; /* waitTime */
 	#endif
 
 	/* Naive gather, out of order */
@@ -65,20 +71,33 @@ int main(int argc, char*argv[]) {
 
 	#ifdef TIME
 	time2 = MPI_Wtime();
-	commTime = time2 - time1;
-	printf("Process %d\n", rank);
-	printf("\tCalc: %lf s\n", calcTime);
-	printf("\tWait: %lf s\n", waitTime);
-	printf("\tComm: %lf s\n", commTime);
+	times[3] = time2 - time1; /* commTime */
+	MPI_Gather(&times, 4, MPI_DOUBLE, allTimes, 4, MPI_DOUBLE, 0, MPI_COMM_WORLD); /* get all timing info */
 	#endif
-
-	if(rank == 0) { 
+	
+	if (rank == 0) {
+		#ifdef TIME
+		time1 = MPI_Wtime();
+		#endif
 		/* Rearrange out-of-place into correct order */
 		for (i = 0; i < procSize; i++) {
 			for (j = 0; j < ncpu; j++) {
 				out[i*ncpu + j] = y[i + j*procSize];  
 			}
 		}
+
+		#ifdef TIME /* Write timing results to .csv file */
+		time2 = MPI_Wtime();
+		times[1] += time2 - time1; /* included added work by process 0 to rearrange */
+
+		fp = fopen("cyclic-results.csv","w");
+		fprintf(fp, "rank,calcTime,waitTime,commTime\n"); /* .csv header */
+		for (i = 0; i < 4*ncpu; i+=4) {
+			fprintf(fp, "%d,%lf,%lf,%lf\n", (int)allTimes[i], allTimes[i+1], allTimes[i+2], allTimes[i+3]);
+		}
+		fclose(fp);
+		free(allTimes);
+		#endif
 		
 		#ifndef TIME
 	    printf("Writing mandelbrot3.ppm\n");
@@ -95,8 +114,8 @@ int main(int argc, char*argv[]) {
 	    
 	    fclose(fp);
 		free(y);
-		free(out);
 		#endif
+		free(out);
 	}
 
 
